13118a70b1c5714cb7eb37689d449f33e7ee9de1
gilgim <waterather21@gmail.com>
2022-01-16 21:52:05 +0900

    CodingTest

---
+++ KimWooJin_CodingTest/KimWooJin_CodingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KimWooJin_CodingTestApp.swift
+//  KimWooJin_CodingTest
+//
+//  Created by KimWooJin on 2022/01/16.
+//
+
+import SwiftUI
+
+@main
+struct KimWooJin_CodingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
